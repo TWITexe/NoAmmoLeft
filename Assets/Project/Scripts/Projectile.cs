ff21873ded8029fe72d9dcb1de8ca9eb9aa2cf45
Даниил Понкратенко <ponkratenko.danil@mail.ru>
2025-10-18 03:32:47 +0300

    feat(bullet) create bullet prefab and controller

---
+++ Assets/Project/Scripts/Projectile.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Projectile : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 10f;
+
+    [SerializeField]
+    private float _lifetime = 2f;
+
+    [SerializeField]
+    private int _damage = 1;
+
+    [SerializeField]
+    private Rigidbody2D _rb;
+
+    private void Awake()
+    {
+        if (_rb == null) 
+            _rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Launch(Vector2 direction)
+    {
+        _rb.linearVelocity = direction * _speed;
+        Destroy(gameObject, _lifetime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Здесь можно повесить урон по врагу
+        other.GetComponent<Health>()?.ApplyDamage(_damage);
+        Destroy(gameObject);
+    }
+}
