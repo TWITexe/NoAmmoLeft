987484b43d6f7ccf9434b795f0bbecc72db57ca9
TWITexe <pkotelnikov2000@gmail.com>
2025-10-18 23:06:34 +0700

    feat(scripts) add timer mechanic

---
+++ Assets/Project/Scripts/UI/Timer.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private float time;
+    [SerializeField] private Image timerImage;
+
+    private float _timeLeft = 0f;
+
+    private IEnumerator StartTimer()
+    {
+        if (_timeLeft <= 0)
+        {
+
+        }
+        while (_timeLeft > 0)
+        {
+            _timeLeft -= Time.deltaTime;
+            var normalizedValue = Mathf.Clamp(_timeLeft / time, 0.0f, 1.0f);
+            timerImage.fillAmount = normalizedValue;
+            yield return null;
+        }
+    }
+
+    private void Start()
+    {
+        _timeLeft = time;
+        StartCoroutine(StartTimer());
+    }
+}
