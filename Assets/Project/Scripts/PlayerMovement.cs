8b5c2d06233d6034c8f0d09c1b9e60a5102b5012
Даниил Понкратенко <ponkratenko.danil@mail.ru>
2025-10-18 03:33:21 +0300

    feat(player): create player move controller

---
+++ Assets/Project/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _moveSpeed = 5f;
+
+    private Rigidbody2D _rb;
+    private Vector2 _moveInput;
+
+    public Vector2 CurrentVelocity => _rb.linearVelocity;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Move(Vector2 input)
+    {
+        _moveInput = input;
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.linearVelocity = _moveInput * _moveSpeed;
+    }
+}
