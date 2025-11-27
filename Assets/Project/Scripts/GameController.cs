fe81359fc91bf6b608a4ec20e5036ca438eff594
TWITexe <kawaii_senpaii@mail.ru>
2025-11-28 02:17:42 +0700

    feat(gameover) added UI for game over

--- Assets/Project/Scripts/GameController.cs
+++ Assets/Project/Scripts/GameController.cs
@@ -1,27 +1,33 @@
 using UI;
+using System;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
-    [SerializeField] GameObject bluePlayer;
-    [SerializeField] GameObject purplePlayer;
+    [SerializeField] private GameObject bluePlaye;
+    [SerializeField] private GameObject purplePlayer;
     [SerializeField] private Timer timer;
     [SerializeField] private BaseSpawner spawner;
     [SerializeField] private TMP_Text winnerText;
-
+    [SerializeField] private Gun bluePlayerGun;
+    [SerializeField] private Gun purplePlayerGun;
     private Health bluePlayerHealth;
     private Health purplePlayerHealth;
-    private Gun bluePlayerGun;
-    private Gun purplePlayerGun;
     private bool blueNoAmmoLeft = false;
     private bool purpleNoAmmoLeft = false;
     private bool endGame = false;
+    private bool noAmmoLeftAll = false;
     private string winText = "NO AMMO LEFT!";
+
+    public event Action NoAmmoLeftAll;
 
     private void Awake()
     {
+        bluePlayerHealth = bluePlaye.GetComponent<Health>();
+        purplePlayerHealth = purplePlayer.GetComponent<Health>();
+
         bluePlayerGun.NoAmmoLeft += BlueNoAmmo;
         purplePlayerGun.NoAmmoLeft += PurpleNoAmmo;
     }
@@ -32,22 +38,26 @@ public class GameController : MonoBehaviour
     }
     private void Update()
     {
-        if ((endGame || ( blueNoAmmoLeft && purpleNoAmmoLeft) || bluePlayerHealth.CurrentHealth <= 0 || purplePlayerHealth.CurrentHealth <= 0))
+        if (blueNoAmmoLeft && purpleNoAmmoLeft)
         {
-            GameOver();
-
-            if (Input.GetKeyDown(KeyCode.R))
-            {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-            }
+            noAmmoLeftAll = true;
+            NoAmmoLeftAll?.Invoke();
+        }
+        if ((endGame == false && (noAmmoLeftAll || bluePlayerHealth.CurrentHealth <= 0 || purplePlayerHealth.CurrentHealth <= 0)))
+        {           
+            GameOver();           
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
 
     }
-
     private void BlueNoAmmo() => blueNoAmmoLeft = true;
     private void PurpleNoAmmo() => purpleNoAmmoLeft = true;
     public void GameOver()
     {
+        endGame = true;
         winnerText.gameObject.SetActive(true);
 
         if (bluePlayerHealth.CurrentHealth <= 0)
